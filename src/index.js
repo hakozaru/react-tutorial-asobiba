5f74543e8c03a618500bc098c4b468f670d540fb
hakozaru <t.morii@everyleaf.com>
2020-10-01 10:41:51 +0900

    Game.jsにリネーム

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import { Game } from "./components/tutorialGame/Components"
+import { Game } from "./components/tutorialGame/Game"
 
 ReactDOM.render(<Game />, document.getElementById("root"));
